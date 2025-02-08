83334611dee868582deb0b3c0d0c596511e9a142
Aliaksandr <alexinforest@gmail.com>
2025-02-08 14:20:28 +0300

    create product ui and action

--- app/auth/login/login.ts
+++ app/auth/login/login.ts
@@ -1,14 +1,17 @@
 "use server";
 
 import { API_URL } from "@/app/common/constants/api";
-import { FormError } from "@/app/common/interfaces/form-error.interface";
+import { FormResponse } from "@/app/common/interfaces/form-response.interface";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import { jwtDecode } from "jwt-decode";
 import { getErrorMessage } from "@/app/common/util/errors";
 import { AUTHENTICATION_COOKIE } from "../auth-cookie";
 
-export default async function login(_prevState: FormError, formData: FormData) {
+export default async function login(
+  _prevState: FormResponse,
+  formData: FormData
+) {
   const res = await fetch(`${API_URL}/auth/login`, {
     method: "POST",
     headers: {
