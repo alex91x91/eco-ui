20cfca90a7eeba0a9645a61882fd8ac2df33a2ed
Aliaksandr <alexinforest@gmail.com>
2025-02-09 00:48:59 +0300

    single product

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,36 @@
+import { Grid2, Stack, Typography } from "@mui/material";
+import getProduct from "./get-product";
+import Image from "next/image";
+import { getProductImage } from "../product-image";
+
+interface SingleProductProps {
+  params: {
+    id: string;
+  };
+}
+export default async function SingleProduct({ params }: SingleProductProps) {
+  const product = await getProduct(+params.id);
+  return (
+    <Grid2 container marginBottom={"2rem"} rowGap={3}>
+      {product.imageExists && (
+        <Grid2 size={{ md: 6, xs: 12 }}>
+          <Image
+            src={getProductImage(product.id)}
+            width={0}
+            height={0}
+            className="w-full sm:w-3/4 h-auto"
+            sizes="100vw"
+            alt="Picture of the product"
+          />
+        </Grid2>
+      )}
+      <Grid2 size={{ md: 6, xs: 12 }}>
+        <Stack gap={3}>
+          <Typography variant="h2">{product.name}</Typography>
+          <Typography>{product.description}</Typography>
+          <Typography variant="h4">${product.price}</Typography>
+        </Stack>
+      </Grid2>
+    </Grid2>
+  );
+}
